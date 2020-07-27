4bd8887190f32d98dcc8971e399f2e2a03d9b28f
Alex Miller <alexmiller@gmail.com>
2020-07-26 22:37:24 -0700

    logo wip

--- logo/src/logo.ts
+++ logo/src/logo.ts
@@ -1,20 +1,77 @@
-import { Engine, Render, Bodies, World } from "matter-js";
+
+import decomp from "poly-decomp";
+import { Engine, Render, Bodies, World, Svg, Vertices } from "matter-js";
+import logoPath from "./logo.svg";
+// import boxPath from "./box.svg";
+import "pathseg";
+
+window.decomp = decomp;
 
 const engine = Engine.create();
+const world = engine.world;
 
 const render = Render.create({
   element: document.body,
-  engine: engine
+  engine: engine,
+  options: {
+    width: window.innerWidth,
+    height: window.innerHeight
+  }
 });
 
-const box = Bodies.rectangle(100, 100, 80, 80);
-const ground = Bodies.rectangle(400, 600, 800, 60, { isStatic: true });
+async function getLogo() {
+  const logoResponse = await fetch(logoPath);
+  const logoSource = await logoResponse.text();
 
-World.add(engine.world, [box, ground]);
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(logoSource, "image/svg+xml");
+  const letters = doc.getElementsByTagName("g")[0].children;
+
+  for (let i = 0; i < letters.length; i++) {
+    console.log(letters[i]);
+    const sections = letters[i].getElementsByTagName("path");
+    for (let j = 0; j < sections.length; j++) {
+      let points = Svg.pathToVertices(sections[j], 2);
+
+      let center = { x: 0, y: 0 };
+      points.forEach(p => {
+        center.x += p.x;
+        center.y += p.y;
+      });
+      center.x /= points.length;
+      center.y /= points.length;
+
+      center.x += 300;
+      center.y += 300;
+
+      const body = Bodies.fromVertices(center.x, center.y, [points], {
+        render: {
+          fillStyle: "#556270",
+          strokeStyle: "#556270",
+          lineWidth: 2
+        }
+      }, true)
+
+      console.log(body.position);
+
+      World.add(engine.world, body);
+    }
+  }
+
+  // let vertexSets = [];
+  // for (let i = 0; i < paths.length; i++) {
+  //   console.log(paths);
+  //   
+  //   vertexSets.push(points);
+  // }
+
+
+
+  // World.add(engine.world,);
+}
+
+getLogo();
+engine.world.gravity = { x: 0, y: 0, scale: 0 };
 
 Engine.run(engine);
 Render.run(render);
-
-console.log(document);
-console.log(document.body)
-console.log(window);
